9304db2fce997696032ead912aa1c458b2436c90
Rohan Mia <rohanmia.org@gmail.com>
2026-02-20 13:01:56 +0600

    created get bonus section

---
+++ script/getBonus.js
@@ -0,0 +1,17 @@
+document.getElementById("bonus-btn").addEventListener("click", function(){
+    //? 1- get the coupon number and match
+    const couponNumber = getValueFromInput("coupon-number")
+    // console.log(couponNumber)
+    if(couponNumber === "Arna Anthonia || Arna corraya || Arna Anthonia Corraya"){
+        //? 2-1 true:: alert you got the bonus 
+        alert("You get a Bonus of 5000 tk")
+        const balance = getBalance()
+        const newBalance = balance + 5000;
+        setBalance(newBalance)
+    }else{
+        //? 2-2 false:: alert invalid coupon and return
+
+        alert("Invalid Coupon")
+        return
+    }
+})
